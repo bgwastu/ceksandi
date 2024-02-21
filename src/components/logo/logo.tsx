a32498423b4e4cc9de0816df9fe1387e9c08d934
Bagas Wastu <67826350+bgwastu@users.noreply.github.com>
2024-02-21 13:25:37 +0700

    fix: custom size for logo

--- src/components/logo/logo.tsx
+++ src/components/logo/logo.tsx
@@ -6,7 +6,7 @@ export default function Logo({ size, ...props }: FlexProps & { size: number }) {
   const iconSize = size * 1.7; // Adjust the scale factor as needed
   return (
     <Flex align="center" gap={2} {...props} className={classes.wrapper}>
-      <Text fz={size} fw="700">
+      <Text fz={size} fw="700" c="gray.7">
         Cek Sandi
       </Text>
       <IconPassword size={iconSize} color="var(--mantine-color-brand-6)" fill="var(--mantine-color-brand-1)" />
